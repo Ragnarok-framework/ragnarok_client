48613ffa4bbe4f03b28b6e54d2c8a6974286985f
LuboNenov <60894956+LuboNenov@users.noreply.github.com>
2021-08-09 17:38:36 +0300

    Add files via upload

---
+++ ragnarok_client/main.py
@@ -0,0 +1,27 @@
+from client import client_socket
+from argparse import ArgumentParser
+
+class Main:
+	def main(self):
+		""" Step 1: executing parse data """
+		parser = ArgumentParser()
+		parser.add_argument("-m", "--mode", dest="mode", type=str, required=True,
+	                    help="SERVER to start a server"
+	                    )
+
+		parser.add_argument("-d", "--debug", dest="debug", required=False,
+	                    help="to print debug messages, enable this option",
+	                    action="store_true"
+	                    )
+
+		args = parser.parse_args()
+
+		if args.debug:
+			print(args)
+			""" Step 2: Getting input for socket data and API definiton """
+		if args.mode.lower() == "client":
+	    	server = "localhost" # input("Server IP: ")
+		    client = Client.ClientSocket(args.debug)
+		    client.start_client(server)
+def run():
+		Main().main()
